22d133a086130cc7cdb1bbf236624423ea79585a
Dmitry <chiruhind@gmail.com>
2023-04-13 20:51:12 +0500

    Add templates and optimize render bridges

--- neuro.py
+++ neuro.py
@@ -10,6 +10,12 @@ import os
 from scipy.optimize import fsolve
 import urllib.request
 from scipy.optimize import newton
+from enum import Enum
+import time
+
+class ConnectionType(Enum):
+    OneCut = 1
+    TwoCuts = 2
 
 def getNormal(r1,r2,r3, coords):
     return np.array([2*coords[0]/r1**2, 2*coords[1]/r2**2, 2*coords[2]/r3**2])
@@ -62,41 +68,41 @@ def getCoordinates(r1, r2, r3):
     "P4":np.array(ellipsoidFormula(r1, r2, r3, r1*math.cos(3*math.pi/5), -r1*math.cos(4*math.pi/5))),
     "F4":np.array(ellipsoidFormula(r1, r2, r3, r1*math.cos(3*math.pi/5), r1*math.cos(4*math.pi/5))),
     }
-    
+      
 def getConnectedPoints():
     return [
-            ("Fp1", "Fp2"),
-            ("Fp1", "F7"),
-            ("F7", "T3"),
-            ("T3", "T5"),
-            ("T5", "O1"),
-            ("Fp2", "F8"),
-            ("F8", "T4"),
-            ("T4", "T6"),
-            ("T6", "O2"),
-            ("O1", "O2"),
-            ("Fp1", "F3"),
-            ("F7", "F3"),
-            ("Fz", "F3"),
-            ("C3", "F3"),
-            ("T3", "C3"),
-            ("Cz", "C3"),
-            ("P3", "C3"),
-            ("T5", "P3"),
-            ("Pz", "P3"),
-            ("O1", "P3"),
-            ("Fp2", "F4"),
-            ("F8", "F4"),
-            ("Fz", "F4"),
-            ("C4", "F4"),
-            ("T4", "C4"),
-            ("Cz", "C4"),
-            ("P4", "C4"),
-            ("T6", "P4"),
-            ("Pz", "P4"),
-            ("O2", "P4"),
-            ("Fz", "Cz"),
-            ("Pz", "Cz")
+            ("Fp1", "Fp2", ConnectionType.TwoCuts),
+            ("Fp1", "F7", ConnectionType.TwoCuts),
+            ("F7", "T3", ConnectionType.TwoCuts),
+            ("T3", "T5", ConnectionType.TwoCuts),
+            ("T5", "O1", ConnectionType.TwoCuts),
+            ("Fp2", "F8", ConnectionType.TwoCuts),
+            ("F8", "T4", ConnectionType.TwoCuts),
+            ("T4", "T6", ConnectionType.TwoCuts),
+            ("T6", "O2", ConnectionType.TwoCuts),
+            ("O1", "O2", ConnectionType.TwoCuts),
+            ("Fp1", "F3", ConnectionType.TwoCuts),
+            ("F7", "F3", ConnectionType.TwoCuts),
+            ("Fz", "F3", ConnectionType.OneCut),
+            ("C3", "F3", ConnectionType.TwoCuts),
+            ("T3", "C3", ConnectionType.TwoCuts),
+            ("Cz", "C3", ConnectionType.TwoCuts),
+            ("P3", "C3", ConnectionType.TwoCuts),
+            ("T5", "P3", ConnectionType.TwoCuts),
+            ("Pz", "P3", ConnectionType.OneCut),
+            ("O1", "P3", ConnectionType.TwoCuts),
+            ("Fp2", "F4", ConnectionType.TwoCuts),
+            ("F8", "F4", ConnectionType.TwoCuts),
+            ("Fz", "F4", ConnectionType.OneCut),
+            ("C4", "F4", ConnectionType.TwoCuts),
+            ("T4", "C4", ConnectionType.TwoCuts),
+            ("Cz", "C4", ConnectionType.TwoCuts),
+            ("P4", "C4", ConnectionType.TwoCuts),
+            ("T6", "P4", ConnectionType.TwoCuts),
+            ("Pz", "P4", ConnectionType.OneCut),
+            ("O2", "P4", ConnectionType.TwoCuts),
+            ("Fz", "Cz", ConnectionType.TwoCuts),
+            ("Pz", "Cz", ConnectionType.TwoCuts)
     ]
 
 def closest_point_on_ellipsoid(point, a, b, c):
@@ -121,33 +127,46 @@ def getPointOnSocketEdge(point, vector, socketRadius, r1, r2, r3):
         point1 = point + vectorInPlane
         return closest_point_on_ellipsoid(point1, r1,r2,r3)
 
-def getCurvesOnEllipsoid(r1, r2, r3, points_coordinates, connections, n = 100):
+def addConnectorPointsToList(point1, point2, type, connectorPoints):
+    if(type == ConnectionType.OneCut):
+        center = (point1 + point2)/2
+        center = closest_point_on_ellipsoid(center, r1,r2,r3)
+        connectorPoints.append([center])
+    elif(type == ConnectionType.TwoCuts):
+        DISTANCE_TILL_CONNECTOR = 12
+        connector_point_vector = normalize(point2-point1) * DISTANCE_TILL_CONNECTOR
+        p1 = closest_point_on_ellipsoid(point1 + connector_point_vector, r1,r2,r3)
+        p2 = closest_point_on_ellipsoid(point2 - connector_point_vector, r1,r2,r3)
+        connectorPoints.append([p1,p2])
+
+def getCurves(r1, r2, r3, points_coordinates, connections, addConnectorPoints = False, n = 100):
     curves = []
+    connectorPoints = []
     socketRadius = 12
 
-    for p1, p2 in connections:
-        print(p1,p2)
+    for p1, p2, type in connections:
         coord1 = points_coordinates[p1]
         coord2 = points_coordinates[p2]
-
         n_vector = (coord2-coord1)/n
 
         point1 = getPointOnSocketEdge(coord1, n_vector, socketRadius,r1,r2,r3)
         point2 = getPointOnSocketEdge(coord2, -n_vector, socketRadius,r1,r2,r3)
-
+        
         points = [point1]
         n_vector = (point2 - point1)/n
         for i in range(1, n):
             points.append(point1 + i*n_vector)
         points.append(point2)
-        print(points)
+
+        if(addConnectorPoints):
+            addConnectorPointsToList(point1, point2, type, connectorPoints)
 
         points_on_ellipsoid = [closest_point_on_ellipsoid(p, r1,r2,r3) for p in points]
 
         curve = Draft.make_bezcurve([App.Vector(x,y,z) for x,y,z in points_on_ellipsoid])
         curves.append(curve.Shape)
 
-    return curves
+    return curves, connectorPoints
 
 def renderSocktes(r1, r2, r3, points_coordinates):
     points_mesh = []
@@ -155,24 +174,25 @@ def renderSocktes(r1, r2, r3, points_coordinates):
     for name,coords in points_coordinates.items():
         point = Mesh.Mesh(config['DEFAULT']['ModelPath'])
         
-        normal = getNormal(r1,r2,r3, coords)
-        normalized_normal = normalize(normal)
-        matrix = rotation_matrix_from_vectors(np.array([0,0,1]), np.array(normalized_normal))
-        angles = R.from_matrix(matrix).as_euler('zyx', degrees=True)
-        point.Placement = App.Placement(App.Vector(coords[0], coords[1], coords[2]),  App.Rotation(-angles[0], angles[1], angles[2]))
+        up = np.array([0,0,1])
+        normal = normalize(getNormal(r1,r2,r3, coords))
+
+        matrix = rotation_matrix_from_vectors(up, normal)
+        quat = R.from_matrix(matrix).as_quat()
+        point.Placement = App.Placement(App.Vector(coords[0], coords[1], coords[2]),  App.Rotation(quat[0], quat[1], quat[2], quat[3]))
         points_mesh.append(point)
         Mesh.show(point)
         
         #draw normals
         # p1 = App.Vector(coords[0], coords[1], coords[2])
-        # p2 = App.Vector(coords[0] + 100 * normalized_normal[0], coords[1] + 100 * normalized_normal[1], coords[2] + 100 * normalized_normal[2])
+        # p2 = App.Vector(coords[0] + 100 * normal[0], coords[1] + 100 * normal[1], coords[2] + 100 * normal[2])
         # Draft.make_line(p1, p2)
     os.remove(config["DEFAULT"]["ModelPath"])
     return points_mesh
 
 def renderBridges(r1, r2, r3, points_coordinates):
     connections = getConnectedPoints()
-    curvesInner = getCurvesOnEllipsoid(r1,r2,r3, points_coordinates, connections)
+    curvesInner, _ = getCurves(r1,r2,r3, points_coordinates, connections)
 
     dr = 5
     r1+=dr
@@ -180,20 +200,31 @@ def renderBridges(r1, r2, r3, points_coordinates):
     r3+=dr
 
     points_coordinates2 = getCoordinates(r1, r2, r3)
-    curvesOuter = getCurvesOnEllipsoid(r1,r2,r3,points_coordinates2, connections)
+    curvesOuter, connectorPoints = getCurves(r1,r2,r3,points_coordinates2, connections, addConnectorPoints=True)
 
     models = []
 
     offsetSize = 2
 
+
     for i in range(len(curvesInner)):
         surface = Part.makeRuledSurface(curvesInner[i], curvesOuter[i])
-        offset1 = surface.makeOffsetShape(offsetSize, 0.1, fill = True)
-        offset2 = surface.makeOffsetShape(-offsetSize, 0.1, fill = True)
-        offset = offset1.fuse(offset2)
+        offset1 = surface.makeOffsetShape(offsetSize, 0.1, fill = False)
+        offset2 = offset1.makeOffsetShape(-2*offsetSize, 0.1, fill = True)
+        offset = offset2 #offset1.fuse(offset2)
         Part.show(offset)
         models.append(offset)
 
+
+    for index, points in enumerate(connectorPoints):
+        for point in points:
+            template = Mesh.Mesh("C:/Users/chiru/AppData/Roaming/FreeCAD/Macro/template.stl")
+            normal = normalize(getNormal(r1,r2,r3, point))
+            matrix = rotation_matrix_from_vectors(np.array([0,0,1]), normal)
+
+            quat = R.from_matrix(matrix).as_quat()
+            template.Placement = App.Placement(App.Vector(point[0], point[1], point[2]),  App.Rotation(quat[0], quat[1], quat[2], quat[3]))
+            Mesh.show(template)
 
 def equations(p):
     x, y, z = p
