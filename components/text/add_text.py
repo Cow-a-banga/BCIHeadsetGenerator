04ffa979c5a1cf7d67b8b36e70b7ef778d62f27b
Dmitry <chiruhind@gmail.com>
2023-06-13 22:32:41 +0500

    new text position

--- components/text/add_text.py
+++ components/text/add_text.py
@@ -14,14 +14,14 @@ def _get_position(params: InputParameters, connection: ConnectorPoint, direction
     point = connection.point
     vector = directionReverse * connection.direction.normalized()
     normal = params.ellipsoid.get_normal(point)
-    new_z = normal.cross(vector).normalized()
-    new_x = normal.cross(new_z).normalized()
-    matrix = np.column_stack((new_x, normal, new_z))
+    new_x = normal.cross(vector).normalized()
+    new_y = normal.cross(new_x).normalized()
+    matrix = np.column_stack((new_x, new_y, normal))
 
     quaternions = R.from_matrix(matrix).as_quat()
-    position = connection.point + (params.bridge.width / 2 * new_z) + (params.bridge.height / 4 * normal)
+    position = connection.point + (params.bridge.height * normal)
 
-    return quaternions, position, params.bridge.text_width * new_z
+    return quaternions, position, (new_x, new_y, params.bridge.text_width * normal)
 
 
 def _generate_text(params: InputParameters, text_str1: str, text_str2: str, connection: ConnectorPoint,
@@ -29,16 +29,21 @@ def _generate_text(params: InputParameters, text_str1: str, text_str2: str, conn
     text1 = Draft.make_shapestring(String=text_str1, FontFile="C:/Windows/Fonts/Arial.ttf", Size=2.0, Tracking=0.0)
     text2 = Draft.make_shapestring(String=text_str2, FontFile="C:/Windows/Fonts/Arial.ttf", Size=2.0, Tracking=0.0)
 
-    width = text2.Shape.BoundBox.XLength
+    width1 = text1.Shape.BoundBox.XLength
+    height1 = text1.Shape.BoundBox.YLength
+    width2 = text2.Shape.BoundBox.XLength
 
-    quat, position, new_z = _get_position(params, connection, directionReverse)
+    quat, position, (new_x, new_y, new_z) = _get_position(params, connection, directionReverse)
     if directionReverse == -1:
-        text1.Placement = App.Placement(App.Vector(position + params.bridge.text_offset * connection.direction), App.Rotation(*quat))
-        text2.Placement = App.Placement(App.Vector(position - (width + params.bridge.text_offset) * connection.direction),
+        text1.Placement = App.Placement(App.Vector(
+            position - new_x * width1 / 2 - new_y * (params.connector.length + height1 + params.bridge.text_offset)),
+                                        App.Rotation(*quat))
+        text2.Placement = App.Placement(App.Vector(position - new_x * width2 / 2 + new_y * params.bridge.text_offset),
                                         App.Rotation(*quat))
     else:
-        text1.Placement = App.Placement(App.Vector(position - params.bridge.text_offset * connection.direction), App.Rotation(*quat))
-        text2.Placement = App.Placement(App.Vector(position + ((width + params.bridge.text_offset) * connection.direction)),
+        text1.Placement = App.Placement(App.Vector(position - new_x * width1 / 2 - new_y * (params.bridge.text_offset + height1)),
+                                        App.Rotation(*quat))
+        text2.Placement = App.Placement(App.Vector(position - new_x * width1 / 2 + new_y * (params.connector.length + params.bridge.text_offset)),
                                         App.Rotation(*quat))
 
     extruded_text1 = Draft.extrude(text1, App.Vector(*new_z))
